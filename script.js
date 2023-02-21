1794be060fcd225d2f3065e35c58019c88b9245d
Nikhith kumar <120315182+Nikhithkumar@users.noreply.github.com>
2023-02-21 19:38:07 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,55 @@
+window.onload = function () {
+    let minutes = 00;
+    let seconds = 00;
+    let tens = 00;
+    let upMinutes = document.getElementById("minutes")
+    let upSeconds = document.getElementById("seconds")
+    let upTens = document.getElementById("tens")
+    let start = document.getElementById("start")
+    let pause = document.getElementById("pause")
+    let reset = document.getElementById("reset")
+    let Interval
+
+    start.onclick = function () {
+        clearInterval(Interval);
+        Interval = setInterval(startTimer, 10)
+    }
+    pause.onclick = function () {
+        clearInterval(Interval)
+    }
+    reset.onclick = function () {
+        clearInterval(Interval)
+        seconds = "00"
+        tens = "00"
+        upSeconds.innerHTML = tens
+        upTens.innerHTML = seconds
+    }
+    function startTimer() {
+        tens++
+
+        if (tens <= 9) {
+            upTens.innerHTML = "0" + tens
+        }
+
+
+        if (tens > 9) {
+            upTens.innerHTML = tens
+        }
+        if (tens > 99) {
+            seconds++
+            upSeconds.innerHTML = "0" + seconds
+            tens = 0
+            upTens.innerHTML = "0" + tens
+        }
+        if (seconds > 9) {
+            upSeconds.innerHTML = seconds
+        }
+        if (seconds > 60) {
+            minutes++
+            upMinutes.innerHTML = "0" + minutes
+            seconds = 0
+            tens = 0
+        }
+    }
+
+}
